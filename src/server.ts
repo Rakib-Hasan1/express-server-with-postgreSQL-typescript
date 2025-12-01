25a90aeae82a15d0ba1db5a09d2db8ad0d881111
MD Rakib Hasan <dev.rakibhasan1@gmail.com>
2025-12-01 11:39:27 +0600

    complete CRUD oparation in postgreSQL

--- src/server.ts
+++ src/server.ts
@@ -95,7 +95,85 @@ app.get("/users", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!!",
+      });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: "User retrived successfully!!",
+        data: result.rows[0],
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!!",
+      });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: "User retrived successfully!!",
+        data: result.rows[0],
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    console.log(result);
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!!",
+      });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: "User deleted successfully!!",
+        data: result.rows,
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
