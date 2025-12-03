b6d5622dac99360a380ab08c29f0573ad8f6a3e2
MD Rakib Hasan <dev.rakibhasan1@gmail.com>
2025-12-03 15:43:32 +0600

    implement jwt to secure authentication for login user

---
+++ src/middleware/auth.ts
@@ -0,0 +1,30 @@
+//* higher order function  -> return korbe function k
+
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+
+const auth = () => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        return res.status(500).json({ message: "You are not allowed!!" });
+      }
+      const decoded = jwt.verify(token, config.jwtSecret as string);
+
+      req.user = decoded as JwtPayload;
+
+      console.log(token);
+      next();
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+};
+
+export default auth;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import initDB from "./config/db";
 import logger from "./middleware/logger";
 import { userRoutes } from "./modules/user/user.routes";
 import { todoRoutes } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 const app = express();
 const port = config.port;
@@ -15,18 +16,18 @@ app.use(express.json());
 // initializing DB
 initDB();
 
-
 app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello Next Level Developers!!!!");
 });
 
-// users CRUD
+//* users CRUD
 app.use("/users", userRoutes);
-
 
 //* todos CRUD
 app.use("/todos", todoRoutes);
 
+//* auth routes
+app.use("/auth", authRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
